4c7b2b489f266b9f4cb7d475c353b5a0fe77e9ad
Aaditya Srivastava <sumionochi@Aadityas-MacBook-Pro.local>
2025-02-10 06:19:27 +0530

    google auth token object is being received

--- src/components/SignInDialog.tsx
+++ src/components/SignInDialog.tsx
@@ -7,6 +7,8 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
+import { useGoogleLogin } from "@react-oauth/google";
+import axios from "axios";
 
 interface SignInDialogProps {
   openDialog: boolean;
@@ -14,39 +16,44 @@ interface SignInDialogProps {
 }
 
 export function SignInDialog({ openDialog, closeDialog }: SignInDialogProps) {
-  const { setUserDetail } = useContext(UserInfoContext);
-
-  const handleGoogleSignIn = () => {
-    // Simulating Google sign in
-    setUserDetail({
-      name: "User",
-      email: "user@example.com",
+    const {userDetail, setUserDetail}=useContext(UserInfoContext);  
+    const googleLogin = useGoogleLogin({
+        onSuccess: async (tokenResponse) => {
+        console.log(tokenResponse);
+        const userInfo = await axios.get(
+            'https://www.googleapis.com/oauth2/v3/userinfo',
+            { headers: { Authorization: `Bearer` + tokenResponse?.access_token } },
+        );
+    
+        console.log(userInfo);
+        setUserDetail(userInfo.data);
+        closeDialog(false);
+        },
+        onError: errorResponse => console.log(errorResponse),
     });
-    closeDialog(false);
-  };
 
-  return (
-    <Dialog open={openDialog} onOpenChange={closeDialog}>
-      <DialogContent className="border-4 border-black dark:border-white bg-white dark:bg-black">
-        <DialogHeader className="space-y-6">
-          <DialogTitle className="text-3xl font-bold">
-            Sign in to Cryptic
-          </DialogTitle>
-          <DialogDescription className="text-lg">
-            Join our community to start obfuscating your code securely.
-          </DialogDescription>
-          <button
-            onClick={handleGoogleSignIn}
-            className="w-full p-4 text-lg font-bold bg-white dark:bg-black border-4 border-black dark:border-white
-              hover:translate-x-[-4px] hover:translate-y-[-4px] active:translate-x-[0px] active:translate-y-[0px]
-              shadow-[4px_4px_0px_0px_rgba(0,0,0)] dark:shadow-[4px_4px_0px_0px_rgba(255,255,255)]
-              hover:shadow-[8px_8px_0px_0px_rgba(0,0,0)] dark:hover:shadow-[8px_8px_0px_0px_rgba(255,255,255)]
-              transition-all duration-200"
-          >
-            Sign in with Google
-          </button>
-        </DialogHeader>
-      </DialogContent>
-    </Dialog>
-  );
+    return (
+        <Dialog open={openDialog} onOpenChange={closeDialog}>
+        <DialogContent className="border-2 rounded-full border-black dark:border-white bg-white dark:bg-black">
+            <DialogHeader className="space-y-4">
+                <DialogTitle className="text-3xl font-bold">
+                    Sign in to Cryptic
+                </DialogTitle>
+                <DialogDescription className="text-lg">
+                    Create an Account to start obfuscating your code securely.
+                </DialogDescription>
+                <button
+                    onClick={() => googleLogin()}
+                    className="w-full p-4 text-lg font-bold bg-white dark:bg-black border-2 border-black dark:border-white
+                    hover:translate-x-[-4px] hover:translate-y-[-4px] active:translate-x-[0px] active:translate-y-[0px]
+                    shadow-[4px_4px_0px_0px_rgba(0,0,0)] dark:shadow-[4px_4px_0px_0px_rgba(255,255,255)]
+                    hover:shadow-[8px_8px_0px_0px_rgba(0,0,0)] dark:hover:shadow-[8px_8px_0px_0px_rgba(255,255,255)]
+                    transition-all duration-200"
+                >
+                    Sign in with Google
+                </button>
+            </DialogHeader>
+        </DialogContent>
+        </Dialog>
+    );
 }
