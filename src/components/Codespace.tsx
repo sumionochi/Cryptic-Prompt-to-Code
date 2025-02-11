893a6781608f2a3ee4e1eeb2e4146f0f404eb45a
Aaditya Srivastava <aaditya.srivastava.connect@gmail.com>
2025-02-12 03:13:30 +0530

    code and preview buttons added

--- src/components/Codespace.tsx
+++ src/components/Codespace.tsx
@@ -5,19 +5,29 @@ import {
   SandpackLayout,
   SandpackCodeEditor,
   SandpackPreview,
+  SandpackFileExplorer,
 } from "@codesandbox/sandpack-react";
 import { useTheme } from 'next-themes';
+import { Button } from './ui/button';
 
 type Props = {}
 
 const Codespace = (props: Props) => {
   const { theme } = useTheme();
+  const [activeTab, setActiveTab] = React.useState('code')
   return (
-    <div className='mt-1'>
+    <div className='mt-1 border h-[100%]'>
+      <div className='bg-white dark:bg-black'>
+        <div className='bg-white flex flex-row gap-0 dark:bg-black'>
+          <Button size={"sm"} variant="ghost" onClick={()=>setActiveTab('code')} className={`rounded-none glassmorphism ${activeTab=='code' && 'text-teal-500'}`}>Code</Button>
+          <Button size={"sm"} variant="ghost" onClick={()=>setActiveTab('preview')} className={`rounded-none glassmorphism ${activeTab=='preview' && 'text-teal-500'}`}>Preview</Button>
+        </div>
+      </div>
       <SandpackProvider template="react" theme={theme === "dark" ? "dark" : "light"}>
         <SandpackLayout>
-          <SandpackCodeEditor />
-          <SandpackPreview />
+          <SandpackFileExplorer style={{height:'83.5vh'}}/>
+          <SandpackCodeEditor style={{height:'83.5vh'}} />
+          <SandpackPreview style={{height:'83.5vh'}} />
         </SandpackLayout>
       </SandpackProvider>
     </div>
